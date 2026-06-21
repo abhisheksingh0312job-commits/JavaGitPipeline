93037d6d84734aa03e5e53bb843d326ca9f7d5ea
AbhishekSingh0312 <abhisheksingh0312@gmail.com>
2026-06-21 14:10:31 +0530

    Added onr file to java

---
+++ src/gitjava/GitFirstProgram.java
@@ -0,0 +1,13 @@
+package gitjava;
+
+public class GitFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("==============Starting the Java Program ===========");
+		System.out.println("Running the Java Program");
+		System.out.println("============Starting the Java Program =============");
+       
+	}
+
+}
